f25a4a6d19da82b12d1530239d664b28b80acd97
Gayathri-Selvaganapathi <82144851+Gayathri-Selvaganapathi@users.noreply.github.com>
2021-04-08 14:06:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<head>
+  <title>Gayathri Selvaganapathi PHP </title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+<h1>A Gayathri Selvaganapathi PHP </h1>
+<?php
+$str = "Gayathri Selvagnapathi";
+echo "The SHA256 value of Gayathri Selvagnapathi is ";
+echo sha1($str);
+?>
+<pre>ASCII ART:
+
+    ***********
+    **       
+    **
+    **       ******
+    **       **  **
+    **       **  **
+    ***********  **
+</pre>
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
+</body>
+</html>
